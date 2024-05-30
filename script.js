c8db8f05db2ff975481d051959e8a8d0683a5350
SMITH07HD <134985521+SMITH07HD@users.noreply.github.com>
2024-05-30 16:47:01 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const fullimg = document.getElementById ("complete");
+fullimg = document.getElementById ("fullscreem");
+
+
+
+function openfullimg (reference)  {
+    
+    fullimg.style.display = "flex"   
+    fulimg.src = reference
+     
+
+}
+
+
+
+function closeimg()  {
+
+
+    fullimg.style.display = "none";
+}
